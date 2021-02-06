8a415e487b6449230cc5dd06558739922c63e807
YUUKIToriyama <ytoriyama@torisfactory.com>
2021-02-07 01:52:46 +0900

    buildディレクトリがない場合にエラーが出る問題を解消

--- src/index.js
+++ src/index.js
@@ -6,11 +6,21 @@ const csv2geojson = require("csv2geojson");
 const loadSources = require("./loadSources");
 
 const buildGeoJSONs = async () => {
-	let srcDir = "../data/";
-	let dstDir = "../build/"
+	let srcDir = "./data/";
+	let dstDir = "./build/"
+
+	// dataフォルダからCSVを読み込む
 	const sources = await loadSources(srcDir);
 	console.log(sources);
 
+	// buildディレクトリがない場合は作成する
+	if (!fs.existsSync("build")) {
+		fs.mkdir("build", (err) => {
+			console.error(err);
+		});
+	}
+
+	// 読み込んだCSVをファイルごとに処理する
 	for (let [fileName, csv] of Object.entries(sources)) {
 		let geojson;
 
@@ -26,6 +36,7 @@ const buildGeoJSONs = async () => {
 			}
 		});
 
+		// 元のファイル名 - ".csv" + ".geojson"でbuildディレクトリに書き出し
 		fs.writeFileSync(dstDir + fileName.replace(/.csv$/, ".geojson"), JSON.stringify(geojson, null, "\t"));
 	}
 }
