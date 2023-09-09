a62619f47a9c1da65ed12764946f88d4812673e6
Yuuki Toriyama <github@toriyama.dev>
2023-09-09 12:20:40 +0900

    update: org.jetbrains.kotlinx:kotlinx-serialization-jsonを追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("multiplatform") version "1.9.10"
+    kotlin("plugin.serialization") version "1.9.10"
 }
 
 repositories {
@@ -10,6 +11,14 @@ kotlin {
     macosX64("native") {
         binaries {
             executable()
+        }
+    }
+    sourceSets {
+        val nativeMain by getting {
+            dependencies {
+                // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-serialization-json
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+            }
         }
     }
 }
